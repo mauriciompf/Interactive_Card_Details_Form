d030a306fec89bfb8a86bd3182005aed3ff07e85
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-07-06 16:26:08 -0400

    Update app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,22 +1,44 @@
 // Data cards selectors
 const frontCardName = document.querySelector(".card-name");
 const frontCardNumber = document.querySelector(".main__cards-number > span");
-const frontCardDate = document.querySelector(".card-date");
+const frontCardDate = document.querySelector(".card-mouth");
+const frontCardYear = document.querySelector(".card-year");
 const backCardCvC = document.querySelector(".main__cards-back > span");
 
 // Inputs selectors
 const inputCardName = document.querySelector("#name");
 const inputCardNumber = document.querySelector("#card-number");
 const inputCardMouth = document.querySelector("#date");
-const inputCardYear = document.querySelector("#inputCardDate");
+const inputCardYear = document.querySelector("#year");
 const inputCardCvc = document.querySelector("#cvc");
 
-// Function to update front card
-function updateFrontCardName(value) {
-  frontCardName.textContent = value;
+// Event listener
+function updateCardContent(event, element) {
+  if (event.target.value.length !== 0) {
+    element.textContent = event.target.value;
+  }
 }
 
-// Event listener
 inputCardName.addEventListener("keyup", (event) => {
-  updateFrontCardName(event.target.value);
+  updateCardContent(event, frontCardName);
+});
+
+inputCardNumber.addEventListener("keyup", (event) => {
+  updateCardContent(event, frontCardNumber);
+  let count = 0;
+  if (event.target.value.length >= 4) {
+    event.target.value += " ";
+  }
+});
+
+inputCardMouth.addEventListener("keyup", (event) => {
+  updateCardContent(event, frontCardDate);
+});
+
+inputCardYear.addEventListener("keyup", (event) => {
+  updateCardContent(event, frontCardYear);
+});
+
+inputCardCvc.addEventListener("keyup", (event) => {
+  updateCardContent(event, backCardCvC);
 });
