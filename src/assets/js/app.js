e66bce8c3e698bf08c145228ea3f254a2f2c52f1
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-07-07 16:11:18 -0400

    Update app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,33 +12,33 @@ const inputCardMouth = document.querySelector("#date");
 const inputCardYear = document.querySelector("#year");
 const inputCardCvc = document.querySelector("#cvc");
 
-// Event listener
-function updateCardContent(event, element) {
-  if (event.target.value.length !== 0) {
-    element.textContent = event.target.value;
+// Completed selectors
+const submitButton = document.querySelector(".main__form-confirm");
+const completedBox = document.querySelector(".main__completed");
+const completedButton = document.querySelector(".main__completed-continue");
+const form = document.querySelector(".main__form-wrap");
+
+// Error messages
+const boxName = document.querySelector(".main__form-box--name");
+const ErrorMessage = document.createElement("p");
+
+submitButton.addEventListener("click", () => {
+  if (inputCardName.value.length === 0) {
+    ErrorMessage.classList.add("error-message");
+    ErrorMessage.textContent = "Can't be blank!";
+    boxName.appendChild(ErrorMessage);
+  } else {
+    completedBox.style.display = "block";
+    form.style.display = "none";
+    frontCardName.textContent = inputCardName.value;
+    frontCardNumber.textContent = inputCardNumber.value;
+    frontCardDate.textContent = inputCardMouth.value;
+    frontCardYear.textContent = inputCardYear.value;
+    backCardCvC.textContent = inputCardCvc.value;
   }
-}
-
-inputCardName.addEventListener("keyup", (event) => {
-  updateCardContent(event, frontCardName);
 });
 
-inputCardNumber.addEventListener("keyup", (event) => {
-  updateCardContent(event, frontCardNumber);
-  let count = 0;
-  if (event.target.value.length >= 4) {
-    event.target.value += " ";
-  }
-});
-
-inputCardMouth.addEventListener("keyup", (event) => {
-  updateCardContent(event, frontCardDate);
-});
-
-inputCardYear.addEventListener("keyup", (event) => {
-  updateCardContent(event, frontCardYear);
-});
-
-inputCardCvc.addEventListener("keyup", (event) => {
-  updateCardContent(event, backCardCvC);
+completedButton.addEventListener("click", () => {
+  completedBox.style.display = "none";
+  form.style.display = "block";
 });
